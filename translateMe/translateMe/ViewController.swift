9ff8cd766477c7b882daf2bfccb72e483bce3933
Kim Mok <cafe.mui@gmail.com>
2016-10-06 00:30:55 -0400

    new swift project

---
+++ translateMe/translateMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  translateMe
+//
+//  Created by Wan Kim Mok on 10/6/16.
+//  Copyright © 2016 Wan Kim Mok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
