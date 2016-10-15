0e2dacece81389d3e55cbe7f839ebb361c179244
Kim Mok <cafe.mui@gmail.com>
2016-10-15 00:59:07 -0400

    documented encodeURIComponent, getAccessToken; and working on translate()

--- translateMe/translateMe/ViewController.swift
+++ translateMe/translateMe/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
 
     @IBAction func translateButton(_ sender: AnyObject) {
         print("translate button pressed")
-        translate(text: "hello girls")
+        translate(text: "hello girls", langTo: "de")
     }
     @IBOutlet var inputText: UITextField!
     
@@ -34,13 +34,62 @@ class ViewController: UIViewController {
     }
     
     
-    func translate(text: String){
-        print(text)
+    /*
+     * Translates
+     *
+     * - Parameters:
+     *      - text   : The string that will be translated
+     *      - langTo : The language `text` will be translated to
+     *
+     * - Returns: The translated text
+     *
+     */
+    func translate(text: String, langTo: String) -> String{
+        //get access token
         getAccessToken();
+        
+        let langFrom = "en";
+        let uri = "https://api.microsofttranslator.com/v2/Http.svc/Translate?text="+text.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)! + "&from=" + langFrom + "&to=" + langTo
+        
+        let authToken = "Bearer " + self.accessToken
+        
+        
+        
+        let url = URL(string: uri)
+        var request = URLRequest(url: url!)
+        
+        request.httpMethod = "GET"
+        request.setValue(authToken, forHTTPHeaderField: "Authorization ")
+
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if ((response as! HTTPURLResponse).statusCode == 200){
+                
+                print(String(data: data!, encoding: String.Encoding.utf8)!)
+                
+                let parser = XMLParser(data: data!)
+                parser.parse()
+               //TODO: WORK ON PARSING XML
+            }
+
+        }
+        
+        task.resume()
+        
+        
+        return "A translated text"
+        
     }
     
 
-    
+    /*
+     * Url-encode strings
+     *
+     * - Parameters:
+     *      - text: The string that will be encoded
+     *
+     * - Returns: The url-encoded string
+     *
+     */
     func encodeURIComponent(text: String) -> String {
         let characterSet = NSMutableCharacterSet.alphanumeric()
         characterSet.addCharacters(in: "-_.!~*'()")
@@ -49,6 +98,9 @@ class ViewController: UIViewController {
     }
 
     
+    /*
+     * Retrieves the access token
+     */
     func getAccessToken(){
         
         let clientId = getKey(key: "bing_client_id")
@@ -57,6 +109,7 @@ class ViewController: UIViewController {
         let grantType = "client_credentials"
         let authUrl = "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13/"
         
+        //format the parameters for the access token
         let params = "client_id=\(encodeURIComponent(text: clientId))&client_secret=\(encodeURIComponent(text: clientSecret))&scope=\(encodeURIComponent(text: scope))&grant_type=\(grantType)"
         
         let url = URL(string: authUrl)
@@ -73,18 +126,16 @@ class ViewController: UIViewController {
                     //convert data to json format
                     let json = try JSONSerialization.jsonObject(with: data!, options: []) as! NSDictionary
                     //set access token
-                    self.accessToken = (json["access_token"] as! String).removingPercentEncoding!
-                }catch{
-                    
-                }
+                    //self.accessToken = (json["access_token"] as! String).removingPercentEncoding!
+                    self.accessToken = (json["access_token"] as! String)
                 
+                }catch{
+                    print("Error converting to json.")
+                }
             }
         }
         
         task.resume()
-
-        
-        
         
         
     }
