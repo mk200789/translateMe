891a26bed55f1b01f210675590cbbdb8a3edda5a
Kim Mok <cafe.mui@gmail.com>
2017-09-18 15:34:22 -0500

    add ui components to screen buttons,label, and imageview

--- translateMe/translateMe/AppDelegate.swift
+++ translateMe/translateMe/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Clarifai_Apple_SDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Clarifai.sharedInstance().start(apiKey: "ce35f782e29646698a096f9b059eb8fb")
         return true
     }
 
